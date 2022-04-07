4bf6746a50e97d63455b265d4f20c84cb2c81ec1
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-04-07 17:57:41 +0530

    write new test cases

---
+++ src/__tests__/Countries.test.tsx
@@ -0,0 +1,33 @@
+/** @format */
+
+import { render, screen } from "@testing-library/react";
+import Countries from "../screens/Countries";
+
+jest.mock("react-router-dom", () => {
+	return {
+		useLocation: () => {
+			return {
+				state: {
+					country: "ind",
+				},
+			};
+		},
+	};
+});
+
+describe("Countries", () => {
+	it("renders", () => {
+		render(<Countries />);
+	});
+
+	it("matches snapshot", () => {
+		const view = render(<Countries />);
+		expect(view).toMatchSnapshot();
+	});
+
+	it("has button enabled", () => {
+		render(<Countries />);
+		const weatherButton = screen.getByRole("button");
+		expect(weatherButton).not.toBeDisabled();
+	});
+});
--- src/__tests__/Home.test.tsx
+++ src/__tests__/Home.test.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Home from "../screens/Home";
 
 jest.mock("react-router-dom", () => {
@@ -17,5 +17,19 @@ describe("Home", () => {
 	it("matches snapshot", () => {
 		const view = render(<Home />);
 		expect(view).toMatchSnapshot();
+	});
+
+	it("has button disabled", () => {
+		render(<Home />);
+		const submitButton = screen.getByRole("button");
+		expect(submitButton).toBeDisabled();
+	});
+
+	it("has button enabled on input", () => {
+		render(<Home />);
+		const input = screen.getByRole("textbox");
+		fireEvent.change(input, { target: { value: "test" } });
+		const submitButton = screen.getByRole("button");
+		expect(submitButton).not.toBeDisabled();
 	});
 });
